3346ddb763e35d980c07f16211c1a8e21ea2bcdb
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2022-10-19 18:19:03 +0300

    [main] another connections

--- src/App.js
+++ src/App.js
@@ -3,25 +3,47 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { useWeb3React } from '@web3-react/core'
 import { InjectedConnector } from "@web3-react/injected-connector";
-import { WalletLinkConnector, WalletConnectConnector } from "@web3-react/walletlink-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { init, useConnectWallet } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+
+const injected = injectedModule()
+
+init({
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum',
+      rpcUrl: 'https://mainnet.infura.io/v3/ab0cd2ee943b4b6c8c5f3e64e24548ff'
+    },
+    {
+      id: '0x4',
+      token: 'rETH',
+      label: 'Rinkeby',
+      rpcUrl: 'https://rinkeby.infura.io/v3/ab0cd2ee943b4b6c8c5f3e64e24548ff'
+    }
+  ]
+ })
 
 export const Injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
 export const CoinbaseWallet = new WalletLinkConnector({
-  url: `https://mainnet.infura.io/v3/$ab0cd2ee943b4b6c8c5f3e64e24548ff`,
+  url: `https://mainnet.infura.io/v3/ab0cd2ee943b4b6c8c5f3e64e24548ff`,
   appName: "Web3-react Demo",
   supportedChainIds: [1, 3, 4, 5, 42],
 });
 
 function App({ context }) {
   const { active, activate, deactivate, library, chainId, account } = useWeb3React();
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
 
   return (
     <div className="App">
-      <Button text="btn" width={100} height={50} onClick={() => activate(CoinbaseWallet, console.log)} />
-      <Button text="btn" width={100} height={50} onClick={() => activate(Injected, console.log)} />
+      <Button text="Connect" width={150} height={50} onClick={() => connect()} />
     </div>
   );
 }
