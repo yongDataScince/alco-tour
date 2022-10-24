f4a679ccd40a65dffc51d4cf7cc7a1bc9c63e9dc
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2022-10-24 22:36:21 +0300

    add base store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.scss';
 import App from './App';
 import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from "@ethersproject/providers";
+import { Provider } from 'react-redux'
+import store from './store'
 
 function getLibrary(provider) {
   return new Web3Provider(provider);
@@ -14,7 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Web3ReactProvider>
   </React.StrictMode>
 );
---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { Contract, ethers } from 'ethers'
+import NFTBox from "../assets/abi/NFTBox.json"
+import NFTBottle from "../assets/abi/NFTBottle.json"
+import BottleStaking from "../assets/abi/BottleStaking.json"
+import Lottery from "../assets/abi/Lottery.json"
+
+
+const initialContractsState = {
+  nftBoxContract: Contract,
+  nftBottleContract: Contract,
+  bottleStakingContract: Contract,
+  lotteryContract: Contract
+}
+
+export const initContracts = createAsyncThunk("contracts/init-contracts", async () => {
+  const provider = await ethers.providers.getDefaultProvider()
+  const nftBoxContract = new ethers.Contract('0x1E53d88f5599D6E297306C645Af00179183e7E64', NFTBox.abi, provider)
+  const nftBottleContract = new ethers.Contract('0xa30a01083d91Ca190870BeC5F063da1560389F71', NFTBottle.abi, provider)
+  const bottleStakingContract = new ethers.Contract('0x4bccb5A735DBb06C187d011ADF82f4109D85692e', BottleStaking.abi, provider)
+  const lotteryContract = new ethers.Contract('0x7Ac5b0cbf28E5CD820ADcB68abAFC50C4249998A', Lottery.abi, provider)
+
+  
+})
+
+export const lotteryResult = createAsyncThunk("contracts/lottery-result", async () => {
+
+})
+
+export const contractsSlice = createSlice({
+  name: 'contracts',
+  initialState: initialContractsState
+})
+
+export default configureStore({
+  reducer: {},
+})
